e1c07479560635f55bccd630c4c6b7ab528cfdb5
Bear <programming@hallhome.us>
2021-07-19 11:48:43 -0500

    Add files via upload

---
+++ database.lua
@@ -0,0 +1,23 @@
+-- AdiBags_Shadowlands_Crafting - Bait Database
+-- Created by N6REJ character is Bearesquishy - dalaran please credit whenever.
+-- Source on GitHub: https://github.com/N6REJ/AdiBags_Shadowlands_Crafting
+--
+local ADDON_NAME, ADDON_TABLE, addon = ...
+
+-- Create addon table
+N = { }
+ADDON_TABLE.N = N
+
+-- Label to use
+N["FilterTitle"] = "Bait"
+
+-- Database of items
+N["database"] = {
+    -- ID,	--Item name
+    173038,	-- Lose Sole bait
+    173039,	-- Iridescent amberjack bait
+    173040,	-- Silvergill pike bait
+    173041,	-- Pocked Bonefish bait
+    173042,	-- Spinefin piranha bait
+    173043,	-- Elysian thade bait
+}
